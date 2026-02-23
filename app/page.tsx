bf83c1fe1c825aa48432fa178e776be554015f07
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-23 21:59:31 +0530

    git add and made folders

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">
+        MedSafe AI – Hospital Level System
+      </h1>
+    </div>
+  );
+}
